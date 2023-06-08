91b6137b94ac31c25ad996a5aaab48891e2a64eb
adambrannon09-jhKgEp <66850339+adam-brannon09@users.noreply.github.com>
2023-06-08 15:15:28 -0500

    add html

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+//API VARIABLES
+let weatherApiRootUrl = 'https://api.openweathermap.org';
+let weatherApiKey = '';
+
+//Page Elements
+let searchBoxEl = document.querySelector('#textarea1');
+let searchBtn = document.querySelector('#search_btn');
+let todayContainer = document.querySelector('#todays_conditions');
+let forecastContainer = document.querySelector('#forecast_container');
+let historyContainer = document.querySelector('#history_container');
+
+//Day.js 
 let date = dayjs().format('M/DD/YYYY');
